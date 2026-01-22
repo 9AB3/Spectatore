719849c33038c1f2c1c8ee72c5315d1366d26fc2
AB <alan.bermingham93@gmail.com>
2026-01-22 21:06:44 +1100

    update

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route, Navigate, Link, useLocation } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { gaPageView } from './lib/analytics';
 import Landing from './pages/Landing';
 import Home from './pages/Home';
